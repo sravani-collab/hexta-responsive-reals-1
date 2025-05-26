c98a7d193199bffaf3f33e42adcdcd64c49e1c67
Dinesh Yerra <68950956+dinokage@users.noreply.github.com>
2025-05-26 11:40:32 +0530

    changed URL - breaks prod

--- src/lib/minio.ts
+++ src/lib/minio.ts
@@ -1,33 +1,33 @@
 export const getProjectImageUrl = (filename: string) => {
-  const baseUrl = "https://s3.rdpdatacenter.in/api/v1/buckets/hexta-dev/objects/download";
+  
   const prefix = `projects/${filename}`;
   const encodedPrefix = encodeURIComponent(prefix);
-  return `${baseUrl}?preview=true&prefix=${encodedPrefix}`;
+  return `${process.env.S3_URL}/${encodedPrefix}`;
 };
 
 export const getTeamImageUrl = (filename: string) => {
-  const baseUrl = "https://s3.rdpdatacenter.in/api/v1/buckets/hexta-dev/objects/download";
+  
   const prefix = `team/${filename}`;
   const encodedPrefix = encodeURIComponent(prefix);
-  return `${baseUrl}?preview=true&prefix=${encodedPrefix}`;
+  return `${process.env.S3_URL}/${encodedPrefix}`;
 };
 
 export const getClientImageUrl = (filename: string) => {
-  const baseUrl = "https://s3.rdpdatacenter.in/api/v1/buckets/hexta-dev/objects/download";
+  
   const prefix = `clients/${filename}`;
   const encodedPrefix = encodeURIComponent(prefix);
-  return `${baseUrl}?preview=true&prefix=${encodedPrefix}`;
+  return `${process.env.S3_URL}/${encodedPrefix}`;
 };
 export const getServiceImageUrl = (filename: string) => {
-  const baseUrl = "https://s3.rdpdatacenter.in/api/v1/buckets/hexta-dev/objects/download";
+  
   const prefix = `services/${filename}`;
   const encodedPrefix = encodeURIComponent(prefix);
-  return `${baseUrl}?preview=true&prefix=${encodedPrefix}`;
+  return `${process.env.S3_URL}/${encodedPrefix}`;
 };
 export const getCareerImageUrl = (filename: string) => {
-  const baseUrl = "https://s3.rdpdatacenter.in/api/v1/buckets/hexta-dev/objects/download";
+  
   const prefix = `career/${filename}`;
   const encodedPrefix = encodeURIComponent(prefix);
-  return `${baseUrl}?preview=true&prefix=${encodedPrefix}`;
+  return `${process.env.S3_URL}/${encodedPrefix}`;
 };
 
