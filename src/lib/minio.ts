27c5a22f976a73b86ad9ee19c12031b9e1405090
Subhadeep <codevizag@gmail.com>
2025-05-26 12:21:40 +0530

    Urge to commit :)

--- src/lib/minio.ts
+++ src/lib/minio.ts
@@ -1,33 +1,31 @@
+const baseURL = "https://cdn.hextasphere.com"
+
 export const getProjectImageUrl = (filename: string) => {
-  const baseURL = "https://cdn.hextasphere.com"
   const prefix = `projects/${filename}`;
   const encodedPrefix = encodeURIComponent(prefix);
   return `${baseURL}/${encodedPrefix}`;
 };
 
 export const getTeamImageUrl = (filename: string) => {
-  const baseURL = "https://cdn.hextasphere.com"
   const prefix = `team/${filename}`;
   const encodedPrefix = encodeURIComponent(prefix);
   return `${baseURL}/${encodedPrefix}`;
 };
 
 export const getClientImageUrl = (filename: string) => {
-  const baseURL = "https://cdn.hextasphere.com"
   const prefix = `clients/${filename}`;
   const encodedPrefix = encodeURIComponent(prefix);
   return `${baseURL}/${encodedPrefix}`;
 };
+
 export const getServiceImageUrl = (filename: string) => {
-  const baseURL = "https://cdn.hextasphere.com"
   const prefix = `services/${filename}`;
   const encodedPrefix = encodeURIComponent(prefix);
   return `${baseURL}/${encodedPrefix}`;
 };
+
 export const getCareerImageUrl = (filename: string) => {
-  const baseURL = "https://cdn.hextasphere.com"
   const prefix = `career/${filename}`;
   const encodedPrefix = encodeURIComponent(prefix);
   return `${baseURL}/${encodedPrefix}`;
 };
-
